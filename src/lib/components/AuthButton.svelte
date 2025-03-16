3e44705cf359b913208ba615f1fa86ac829075a1
HIBIKI <hibiki.apple.1984@gmail.com>
2025-03-17 00:49:32 +0900

    Add: 処理中の表示を追加

--- src/lib/components/AuthButton.svelte
+++ src/lib/components/AuthButton.svelte
@@ -1,49 +1,60 @@
-<script lang='ts'>
+<script lang="ts">
   import type { VerifiedRegistrationResponse } from '@simplewebauthn/server';
   import type { PublicKeyCredentialRequestOptionsJSON } from '@simplewebauthn/types';
-  import type { InferSelectModel } from 'drizzle-orm';
+  import { is, type InferSelectModel } from 'drizzle-orm';
   import { invalidateAll } from '$app/navigation';
   import { users } from '$lib/db/schema';
   import { startAuthentication } from '@simplewebauthn/browser';
 
-  const { user }: { user: InferSelectModel<typeof users> | undefined } = $props();
+  const { user }: { user: InferSelectModel<typeof users> | undefined } =
+    $props();
 
   async function login() {
-    const { options } = await (await fetch(`/api/auth/login`)).json().catch((err) => {
-      console.error(err);
-    }) as { options: PublicKeyCredentialRequestOptionsJSON | null };
-    if (!options)
-      return;
+    isProcessing = true;
+    const { options } = (await (await fetch(`/api/auth/login`))
+      .json()
+      .catch((err) => {
+        console.error(err);
+      })) as { options: PublicKeyCredentialRequestOptionsJSON | null };
+    if (!options) return;
     const authenticationResponse = await startAuthentication({
       optionsJSON: options,
     });
 
-    const verificationJSON: VerifiedRegistrationResponse = await (await fetch('/api/auth/login/verify-challenge', {
-      method: 'POST',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(authenticationResponse),
-    })).json();
+    const verificationJSON: VerifiedRegistrationResponse = await (
+      await fetch('/api/auth/login/verify-challenge', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(authenticationResponse),
+      })
+    ).json();
 
     if (verificationJSON.verified) {
       await invalidateAll();
-    }
-    else {
+      isProcessing = false;
+    } else {
       throw new Error(`Verification failed: ${verificationJSON}`);
     }
   }
 
   async function logout() {
+    isProcessing = true;
     await fetch('/api/auth/logout', {
       credentials: 'same-origin',
     });
     await invalidateAll();
+    isProcessing = false;
   }
+
+  let isProcessing = $state(false);
 </script>
 
-{#if user?.id}
+{#if isProcessing}
+  <span>処理中...</span>
+{:else if user?.id}
   <button onclick={logout}>ログアウトする</button>
 {:else}
   <button onclick={login}>ログインする</button>
